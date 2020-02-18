ce9b92c0977ad6b634734983f836fdbee0e1bdfd
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2020-02-18 20:45:41 +0530

    added pending migrations

---
+++ pendingMigrations/20200218102543-cient_update.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn("clients", "blacklisted", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
